2916ea850a8de792cb295575f6de6d4f924e98c2
scottsilver <scottsilver47@gmail.com>
2020-06-24 11:27:49 -0500

    Updates

---
+++ precache-manifest.9b91eb078a72976c80d12d63e56de3c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2ed72deb3fea0241155648cd000494",
+    "url": "/mintbean-weatherapp/index.html"
+  },
+  {
+    "revision": "64e284eb0f62b8686c36",
+    "url": "/mintbean-weatherapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed25a406cfb02d160093",
+    "url": "/mintbean-weatherapp/static/js/2.e60dfb05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mintbean-weatherapp/static/js/2.e60dfb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e284eb0f62b8686c36",
+    "url": "/mintbean-weatherapp/static/js/main.19ae4913.chunk.js"
+  },
+  {
+    "revision": "43c7e5f55c74c5e91e0c",
+    "url": "/mintbean-weatherapp/static/js/runtime-main.98f2f63d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mintbean-weatherapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
